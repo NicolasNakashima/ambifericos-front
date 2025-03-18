ada14f1546387eef521d9a6d39a4d247f4f94dcb
PedroSchettini <pedro.sousa@germinare.org.br>
2025-03-18 10:56:37 -0300

    subindo

--- src/components/ProductCard/style.ts
+++ src/components/ProductCard/style.ts
@@ -4,27 +4,27 @@ export const Card = styled.div(
   () => css`
     width: 200px;
     height: 350px;
-    background-color: green;
     display: flex;
     flex-direction: column;
     border-radius: 1rem;
+    overflow: hidden;
   `
 );
 
 export const ImageCard = styled.img(
   () => css`
     width: 100%;
-    height: 40%;
+    height: 80px;
   `
 );
 
 export const CardInformations = styled.div(
   () => css`
     width: 100%;
-    height: 100%;
     display: flex;
+    background-color: green;
     flex-direction: column;
-    margin: 4px;
+    padding: 4px;
   `
 );
 
